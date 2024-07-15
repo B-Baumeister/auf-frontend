8b8baf8bc95fada01a4a4ebcefa825c7cf6dabdf
Siegfried Margin <siegfried.margin@gmail.com>
2024-07-15 12:47:57 +0200

    added: different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    TableComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
